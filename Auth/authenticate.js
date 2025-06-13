15697d2b4dce4e05c3397ac1e47d1185f137f060
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-06-13 08:40:45 +0100

    Update authenticate.js

--- Auth/authenticate.js
+++ Auth/authenticate.js
@@ -332,7 +332,7 @@ router.get("/paystack/callback", async (req, res) => {
 
       // ✅ Automatically call the verification route on the backend
       const verifyResponse = await axios.post(
-        "https://rendereleven-giq9.onrender.com/paystack/verify",
+        "https://renderelevennew.onrender.com/paystack/verify",
         { reference, userId },
         { headers: { "Content-Type": "application/json" } }
       );
