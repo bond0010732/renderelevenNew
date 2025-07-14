64a5281458ef6bbf3279dee3d45bf3a62f52f52b
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-07-14 05:32:47 +0100

    Update authenticate.js

--- Auth/authenticate.js
+++ Auth/authenticate.js
@@ -877,7 +877,7 @@ router.post('/register', upload.single('image'), registrationLimiter, async (req
     const otp = Math.floor(1000 + Math.random() * 9000).toString();
 
       // Create JWT
-    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+    //const token = jwt.sign({ userId: user._id }, JWT_SECRET);
 
     console.log(otp,'jdjd')
     // Save temporary user (Unverified)
