157c2692e742badd9335a6a7c7c3c0486671a2a3
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-22 11:50:11 +0100

    Update AddTimeLog.js

--- models/AddTimeLog.js
+++ models/AddTimeLog.js
@@ -2,19 +2,10 @@
 const mongoose = require('mongoose');
 
 const AddTimeLogSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'OdinCircledb', // Make sure this matches your user model name
-    required: true
-  },
-  cost: {
-    type: Number,
-    required: true
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
+   userId: { type: mongoose.Schema.Types.ObjectId, ref: "OdinCircledbModel", required: true },
+  cost: { type: Number, required: true },
+  type: { type: String, enum: ["50", "call", "unlock_access","image","video","giphy","other"], required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('AddTimeLog', AddTimeLogSchema);
