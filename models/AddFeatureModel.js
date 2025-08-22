4b9b191aa7f9e3b08ccff7dd3cf9757ae3aef925
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-22 12:04:51 +0100

    Create AddFeatureModel.js

---
+++ models/AddFeatureModel.js
@@ -0,0 +1,11 @@
+// models/AddTimeLog.js
+const mongoose = require('mongoose');
+
+const AddFeatureSchema = new mongoose.Schema({
+   userId: { type: mongoose.Schema.Types.ObjectId, ref: "OdinCircledbModel", required: true },
+  cost: { type: Number, required: true },
+  type: { type: String, enum: ["50", "call", "unlock_access","image","video","giphy","other"], required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('AddFeature', AddFeatureSchema);
