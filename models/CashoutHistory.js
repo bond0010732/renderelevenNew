681691cd3cbfe6abc0ff178802c4c0e6f813aa56
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-22 19:42:33 +0100

    Update CashoutHistory.js

--- models/CashoutHistory.js
+++ models/CashoutHistory.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const CashoutHistorySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "OdinCircledb", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "OdinCircledbModel", required: true },
   amount: { type: Number, required: true },
   type: { type: String, enum: ["credit", "debit"], default: "credit" }, // credit = add, debit = withdraw
   createdAt: { type: Date, default: Date.now }
