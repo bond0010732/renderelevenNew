77d799e649f5b0402a05d55a744a8d42a1147979
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-22 11:41:14 +0100

    Update BetModelQuiz.js

--- models/BetModelQuiz.js
+++ models/BetModelQuiz.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const BetQuizSchema = new mongoose.Schema({
    userId: { type: mongoose.Schema.Types.ObjectId, ref: "OdinCircledbModel", required: true },
   batchId: { type: mongoose.Schema.Types.ObjectId, ref: "BatchModel", required: true },
-  type: { type: String, enum: ["bet", "win", "loss", "withdraw", "deposit"], required: true },
+  type: { type: String, enum: ["triviaBet", "win", "lost"], default: "triviaBet" },
   amount: { type: Number, required: true },
   balanceAfter: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
